160b8a24bee745a4898b5517a27485bbcad36489
Jormin <jorminxee@gmail.com>
2018-11-05 11:38:45 +0800

    修改服务端监听IP
    	modified:   src/console/client.php
    	modified:   src/console/server.php

--- src/console/client.php
+++ src/console/client.php
@@ -6,5 +6,5 @@ use Jormin\Swooler\Client\WebsocketClient;
 
 require_once __DIR__.'/index.php';
 
-$client = new WebsocketClient('127.0.0.1', 666);
+$client = new WebsocketClient('0.0.0.0', 9999);
 $client->connect();
--- src/console/server.php
+++ src/console/server.php
@@ -6,5 +6,6 @@ use Jormin\Swooler\Server\WebsocketServer;
 
 require_once __DIR__.'/index.php';
 
-$webSocketClient = new WebsocketServer('127.0.0.1', 666);
+// 由Docker映射出去端口，这里监听IP一定要注意写 0.0.0.0，否则外部客户端连接会出现未收到握手消息前就断开连接
+$webSocketClient = new WebsocketServer('0.0.0.0', 9999);
 $webSocketClient->start();
